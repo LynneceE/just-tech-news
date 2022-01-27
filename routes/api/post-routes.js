61ab6e79fa55a990e5eca51fce9e34261091db0f
bhari eutsey <bharilynn@outlook.com>
2022-01-27 02:53:18 -0600

    trying to fix errors

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,6 @@ const { Post, User } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-    console.log('======================');
     Post.findAll({
       // Query configuration
       attributes: ['id', 'post_url', 'title', 'created_at'],
