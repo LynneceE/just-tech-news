b103aaf31997358065dce85c40b1ce628854992e
bhari eutsey <bharilynn@outlook.com>
2022-02-03 12:28:34 -0600

    added comment.js, and routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+const homeRoutes = require('./home-routes.js'); // pulled from controller file
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+
+
+// connect public files to backend
+const path = require('path');
+
+
+
 // basic set up for dependencies
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers'); // once routes folder rename to controller, update
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,9 +15,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); //built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets. 
 
 // turn on routes
 app.use(routes);
+
+// set up Handlebars.js as app template engine (installed by running install express-handlebars)
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
